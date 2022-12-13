07eaa2028b563f54b789c1380f3bac88e73b1f40
Aidan Veach <veachaidanjacob@gmail.com>
2022-12-13 14:35:27 -0500

    submit event listener

--- script.js
+++ script.js
@@ -13,4 +13,16 @@ window.addEventListener("load", function() {
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
    
+   let form = document.getElementById("launchForm");
+
+   form.addEventListener("submit", function(event){
+    event.preventDefault();
+    let pilotInput = document.getElementById("pilotName");
+    let copilotInput = document.getElementsByName("copilotName");
+    let fuelLevelInput = document.getElementsByName("fuelLevel");
+    let cargoMassInput = document.getElementsByName("cargoMass");
+    let list = document.getElementById("faultyItems");
+    formSubmission(document, list, pilotInput.value, copilotInput.value, fuelLevelInput.value, cargoMassInput.value);
+   })
 });
+
